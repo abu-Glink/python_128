48e83ead5f31d00c9252e38469787b9357110365
abu-Glink <sayed123729@gmail.com>
2024-12-16 10:27:50 +0600

    first commit

---
+++ c1.py
@@ -0,0 +1,27 @@
+# print("hello world")
+# x= "hello bangladesh"
+# x = 4
+# x = "abu"
+# x="hello bangladesh"
+# x = 10
+# x = 10.5
+# x = 2j
+# x = memoryview(bytes(5))
+# x = b"hello"
+# x = bytearray(123)
+# x = memoryview(bytes(12312342))
+# x = "hello bnagladesh"
+# x = [ "a" "b" "c" "d" ]
+# x = ("apple", "banana", "cherry")
+# x = frozenset({"apple", "banana", "cherry"})
+# x = b"hello"
+# x = bytearray(345674)
+# x = memoryview(bytes(4543564765))
+x = 5
+y = "John"
+print(x)
+print(y)
+
+print(type(x))
+print(type(x))
+print(type(x))
